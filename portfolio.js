8d9d44d109f940b81be9a44e171f3dcba474d5fb
Prince Kumar <128849762+prince10715@users.noreply.github.com>
2025-09-26 22:23:35 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,10 @@
+var typed = new Typed(".text", {
+    strings: ["Java Development", "Android Development"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+
+
